243572558ed8167cf19b037e4e5c69d50bd00f87
abolraj <abolfazlrajaeenasab@gmail.com>
2024-12-15 22:06:53 +0330

    fix - Add role logic

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -30,15 +30,18 @@ class RegisteredUserController extends Controller
      */
     public function store(Request $request): RedirectResponse
     {
+        // dd($request->input());
         $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|string|lowercase|email|max:255|unique:'.User::class,
+            'role' => 'required|boolean',
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
+            'role' => !!$request->role,
             'password' => Hash::make($request->password),
         ]);
 

bf983606265459bb5a1407838bdf3402a4a2f15c
abolraj <abolfazlrajaeenasab@gmail.com>
2024-12-18 19:04:23 +0330

    fix - Fix box structure

--- resources/js/Components/UserItem.jsx
+++ resources/js/Components/UserItem.jsx
@@ -12,12 +12,12 @@ export default forwardRef(function UserItem(
         <section
             {...props}
             className={
-                'cause-item h-20 rounded-md border-2 border-blue-700 shadow-sm focus:border-indigo-500 dark:border-blue-700 dark:bg-gray-900 dark:text-gray-300 p-2 flex flex-wrap items-center gap-2 ' +
+                'cause-item rounded-md border-2 border-blue-700 shadow-sm focus:border-indigo-500 dark:border-blue-700 dark:bg-gray-900 dark:text-gray-300 p-2 flex flex-wrap items-center gap-2 ' +
                 className
             }
             ref={localRef}
         >
-            <div className="justify-between gap-2 h-full aspect-square text-blue-700">
+            <div className="justify-between gap-2 h-16 aspect-square text-blue-700">
                 <User />
             </div>
             <div className="cause.description bg-gray-900 text-blue-500">
@@ -27,6 +27,8 @@ export default forwardRef(function UserItem(
                 <br />
                 {user.email}
             </div>
+
+            {props.children}
         </section>
     );
 });

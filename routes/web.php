d3374c23d8560aafc3588dfa65ec75726fe87b55
abolraj <abolfazlrajaeenasab@gmail.com>
2024-12-18 19:03:02 +0330

    fix - Fix controller class of donation anme

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CauseController;
+use App\Http\Controllers\DonationController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -26,5 +27,6 @@ Route::middleware('auth')->group(function () {
 });
 
 Route::resource('causes', CauseController::class);
+Route::resource('causes.donations', DonationController::class);
 
 require __DIR__.'/auth.php';

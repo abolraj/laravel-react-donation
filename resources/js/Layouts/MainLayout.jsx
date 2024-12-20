cf30b27755f93d4b5a1eedf34f0695db98289c1c
abolraj <abolfazlrajaeenasab@gmail.com>
2024-12-20 12:56:01 +0330

    feat - Add Main layout for pages

---
+++ resources/js/Layouts/MainLayout.jsx
@@ -0,0 +1,32 @@
+import Footer from '@/Components/Footer';
+import Header from '@/Components/Header';
+import Main from '@/Components/Main';
+import Sidebar from '@/Components/Sidebar';
+import IndexCause from '@/Pages/Cause/IndexCause';
+import { Link, usePage } from '@inertiajs/react';
+import { useState } from 'react';
+
+export default function MainLayout({ children, sidebar = null, ...props }) {
+    const auth = usePage().props.auth;
+
+    return (
+        <div className="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50 background-pattern">
+            <div className="relative flex min-h-screen flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
+                <div className="relative w-full max-w-2xl px-6 lg:max-w-7xl">
+                    <Header auth={auth} />
+
+                    {sidebar !== false &&
+                        <Sidebar>
+                            {sidebar}
+                        </Sidebar>
+                    }
+                    <Main>
+                        {children}
+                    </Main>
+
+                    <Footer />
+                </div>
+            </div>
+        </div>
+    );
+}

c9d3cc340f3f3f654b2485aa367dea002e172cf7
abolraj <abolfazlrajaeenasab@gmail.com>
2024-12-20 12:54:19 +0330

    feat - Share the all of donations value in frontend componentss

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\Donation;
 use Illuminate\Http\Request;
 use Inertia\Middleware;
 
@@ -34,6 +35,7 @@ class HandleInertiaRequests extends Middleware
             'auth' => [
                 'user' => $request->user(),
             ],
+            'donations_amount' => Donation::get_all_donations_amount_formatted(),
         ];
     }
 }

9236398b19c1fafb866d8137690b4b855c760509
abolraj <abolfazlrajaeenasab@gmail.com>
2024-12-21 16:34:56 +0330

    feat - Get donation relation

--- app/Models/PaymentTransaction.php
+++ app/Models/PaymentTransaction.php
@@ -3,12 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class PaymentTransaction extends Model
 {
     public $fillable = [
         'donation_id',
+        'transaction_id',
         'status',
         'payment_method',
     ];
+
+    public function donation():BelongsTo{
+        return $this->belongsTo(Donation::class, "donation_id")->withoutGlobalScope('verifieds');
+    }
 }
